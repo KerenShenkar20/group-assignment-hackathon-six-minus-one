3069fa1cd31f5d36c225de57a60c7da6c1d8e790
Sagi <chuboksagi@gmail.com>
2020-12-18 12:22:06 +0200

    fix front

--- Client/js/main.js
+++ Client/js/main.js
@@ -19,6 +19,9 @@ function recreateTable(match) {
 
 function operationsListeners() {
     $("#regVol").click(() => {
-  
+        const id = $("#inputId").val();
+        const fn = $("#inputFN").val();
+        const ln = $("#inputLN").val();
+        const email = $("#inputEmail").val();
     });
 }

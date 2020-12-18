673cce52c1847b2d22c4c0fca4dbb059eeb85af6
Timorsa <timor.saf@gmail.com>
2020-12-18 13:30:06 +0200

    added server -> client serving

--- server.js
+++ server.js
@@ -12,14 +12,23 @@ const port = process.env.PORT || 3000;
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + '/Client/')));
+
+// @serve client side 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Client/index.html'));
+});
+
+
 // app.use(express.static(path.join(__dirname + '/client/')));
- 
+
 // app.use((req,res,next)=>{
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 //  });
- 
+
 // @serve client side 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/client/index.html'));
@@ -31,6 +40,8 @@ app.use('/api/match', matchRouter);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.status(500).send('Something is broken!');
 });
 

965d2a680b8fe261241eb37c0ac4fab78cb3cfd0
Adi Yaari <adiyaari01@gmail.com>
2020-12-18 12:04:44 +0200

    router

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -1,3 +1,11 @@
 const { Router } = require ('express');
-const {userDbController} = require ('../controllers/user.ctrl');
+const {userDbController} = require ('../Controllers/user.ctrl');
 const userRouter = new Router();
+
+userRouter.get('/', userDbController.getUsers);
+userRouter.get('/:id', userDbController.getUser);
+userRouter.post('/', userDbController.addUser);
+
+
+
+module.exports = {userRouter};
